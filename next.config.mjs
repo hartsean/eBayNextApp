0b4a622ea894ce17be4fa9b89528c2df763f0fa6
Sean Hart <hartsean@gmail.com>
2024-04-29 17:40:54 -0500

    configured types for env variables, print item title and description to page.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+webpack: (config) => {
+    config.experiments = {
+      topLevelAwait: true,
+    }
+    return config
+  }
+
 const nextConfig = {};
 
 export default nextConfig;
