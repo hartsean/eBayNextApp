22722adecb27756ebf4822197229d5a5e4ca6007
hartsean <hartsean@gmail.com>
2024-04-29 17:54:31 -0500

    Ebay api integration (#1)
    * (feature) installed ebay-api npm package
    
    * (feat) create test file for ebay-api-test.ts
    
    * (chore) installed dotenv pckg.
    
    * added gitignore file
    
    * add env variables
    
    * configured types for env variables, print item title and description to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,34 @@ Make a GET Request to Inventory API, to get all items.
 
 https://api.ebay.com/sell/inventory/v1/inventory_item?limit=2&offset=0
 
-Error: Needs access token in header. 
 
 */
 
+import eBayApi from 'ebay-api';
+import 'dotenv/config'
+// or:
+// const eBayApi = require('ebay-api')
+
+const eBay = new eBayApi({
+  appId: process.env.EBAY_APP_ID,
+  certId: process.env.EBAY_CERT_ID,
+  sandbox: false
+});
+
+const item = await eBay.buy.browse.getItem('v1|166720018589|0');
+
+console.log('typeof item', typeof item);
+console.log('data bitch', JSON.stringify(item, null, 2));
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello Worlds
+      <div>
+        <div className="book-title">{item.title}</div>
+        <div className="book-description">{item.shortDescription}</div>
+        <div className="book-price-value">{item.price.value}</div>
+        <div className="book-price-currency">{item.price.currency}</div>
+
       </div>
     </main>
   );
---
+++ env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+      // 👇 Replace with your ENV names and types
+      EBAY_APP_ID: string;
+      EBAY_CERT_ID: string;
+      EBAY_DEV_ID: string;
+    }
+  }
